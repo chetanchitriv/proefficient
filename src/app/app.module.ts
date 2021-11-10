4b41ad0377095348f95cac0e140a0a36beaba20c
monika randive <randivemonika2017@gmail.com>
2021-11-10 14:42:02 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { TopnavComponent } from './shared/topnav/topnav.component';
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    TopnavComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
